dd479f08b4efe28ada34341d47b302a6c1eb111f
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-05-21 16:11:03 -0600

    Flags and pointer receivers added

--- quiz/main.go
+++ quiz/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
@@ -11,11 +12,17 @@ import (
 	"time"
 )
 
-func main() {
-	in := bufio.NewReader(os.Stdin)
+type Quiz struct {
+	filename       string
+	timeLimit      int
+	shuffle        bool
+	problems       [][]string
+	totalScore     int
+	totalQuestions int
+}
 
-	//Opening and reading question file
-	csvfile, err := os.Open("problems.csv")
+func (q *Quiz) readProblems() {
+	csvfile, err := os.Open(q.filename)
 	if err != nil {
 		log.Fatalln("Couldn't open csv file ", err)
 	}
@@ -24,39 +31,51 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	q.problems = records
+}
+func (q *Quiz) shuffleProblems() {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(q.problems), func(i, j int) { q.problems[i], q.problems[j] = q.problems[j], q.problems[i] })
+}
 
-	//Asking shuffle and time limit choice
-	var totalScore, totalQuestions, timeLimit, timeChoice int
-	var shuffle string
-	shuffleOptions := map[string]bool{"y": true, "n": false}
-	fmt.Println("Do you want to shuffle the questions ? y or n")
-	fmt.Scanln(&shuffle)
-	if shuffleOptions[shuffle] {
-		rand.Seed(time.Now().UnixNano())
-		rand.Shuffle(len(records), func(i, j int) { records[i], records[j] = records[j], records[i] })
-	}
-	timeOptions := map[int]int{1: 30, 2: 45, 3: 60}
-	fmt.Println("Choose time limit \n 1. 30 seconds \t 2. 45 seconds \t 3. 60 seconds")
-	fmt.Scanln(&timeChoice)
-
-	//Timer begins
-	timeLimit = timeOptions[timeChoice]
-	timer := time.AfterFunc(time.Second*time.Duration(timeLimit), func() {
-		fmt.Println("Total Score : ", totalScore)
-		fmt.Println("No. of Questions : ", totalQuestions)
-		os.Exit(0)
-	})
-	defer timer.Stop()
-	for i := 0; i < len(records); i++ {
-		record := records[i]
-		fmt.Println("What is " + record[0] + " ?")
+func (q *Quiz) printProblems() {
+	in := bufio.NewReader(os.Stdin)
+	for i := 0; i < len(q.problems); i++ {
+		question, answer := q.problems[i][0], q.problems[i][1]
+		fmt.Println("What is " + question + " ?")
 		userAnswer, _ := in.ReadString('\n')
 
 		//Ignores case mismatch and additional white spaces
-		if strings.Join(strings.Fields(strings.ToLower(userAnswer)), "") == strings.ToLower(record[1]) {
-			totalScore++
+		if strings.Join(strings.Fields(strings.ToLower(userAnswer)), "") == strings.ToLower(answer) {
+			q.totalScore++
 		}
-		totalQuestions++
+		q.totalQuestions++
 	}
+}
+
+func main() {
+
+	//Get flags values
+	filename := flag.String("filename", "problems.csv", "File containing the questions")
+	timeLimit := flag.Int("timeLimit", 30, "Time limit in seconds")
+	shuffle := flag.Bool("shuffle", false, "Shuffle questions")
+	flag.Parse()
+
+	q := Quiz{filename: *filename, timeLimit: *timeLimit, shuffle: *shuffle}
+
+	q.readProblems()
+	if q.shuffle {
+		q.shuffleProblems()
+	}
+
+	//Setting timer
+	timer := time.AfterFunc(time.Second*time.Duration(q.timeLimit), func() {
+		fmt.Println("Total Score : ", q.totalScore)
+		fmt.Println("No. of Questions : ", q.totalQuestions)
+		os.Exit(0)
+	})
+	defer timer.Stop()
+
+	q.printProblems()
 
 }
